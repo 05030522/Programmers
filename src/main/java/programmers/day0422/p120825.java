f35121a251096000f5da9de1f41f8257a068095a
05030522 <kjinhx96@gmail.com>
2024-04-22 15:58:52 +0900

    init

---
+++ src/main/java/programmers/day0422/p120825.java
@@ -0,0 +1,12 @@
+package programmers.day0422;
+
+public class p120825 {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
