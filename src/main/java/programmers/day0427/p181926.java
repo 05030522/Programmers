3124fbc3b5ad785c456e7d0a86ea18673bd13c24
05030522 <kjinhx96@gmail.com>
2024-04-29 15:24:40 +0900

    lv.0 04/29

--- src/main/java/programmers/day0427/p181926.java
+++ src/main/java/programmers/day0427/p181926.java
@@ -4,15 +4,14 @@ public class p181926 {
     public int solution(int n, String control) {
         int answer = 0;
         for(int i=0; i<control.length(); i++){ //string의 길이는 length() 괄호기억해!
-            switch(control.charAt(i)){
-                case 'w' : n+=1;
-                    break;
-                case 's' : n-=1;
-                    break;
-                case 'd' : n+=10;
-                    break;
-                case 'a' : n-=10;
-                    break;
+            if (control.charAt(i) == 'w') {
+                n += 1;
+            } else if (control.charAt(i) == 's') {
+                n -= 1;
+            } else if (control.charAt(i) == 'd') {
+                n += 10;
+            } else if (control.charAt(i) == 'a') {
+                n -= 10;
             }
         }
         return n;
