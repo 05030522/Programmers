60b2a632f28cb8c00f7471aca8a91bcf8baab82b
05030522 <kjinhx96@gmail.com>
2024-05-10 22:46:49 +0900

    lv.0 05/10

---
+++ src/main/java/programmers/mon_5/dya0510/p120904.java
@@ -0,0 +1,19 @@
+package programmers.mon_5.dya0510;
+
+public class p120904 {
+    public int solution(int num, int k) {
+        int answer =-1;
+        String numString = Integer.toString(num);
+        int[] temp = new int[numString.length()];
+        for (int i = 0; i <numString.length(); i++) {
+            temp[i]=numString.charAt(i);
+        }
+        for (int i = 0; i <temp.length; i++) {
+            if(temp[i]==(char)(k+'0')){ //문자로 표현할때 사용하는 방법, 정수값 k로 쓰고 싶다면 '0'을 쪽을 제거하면 됨
+                answer=i+1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
